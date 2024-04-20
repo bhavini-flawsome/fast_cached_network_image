d8034549a9c705e2ea7ba8c07beb54f7a1618896
rushabhnavadiya <122771122+bhavini-flawsome@users.noreply.github.com>
2024-04-20 22:38:33 +0530

    Add svg support

--- lib/src/fast_cached_image.dart
+++ lib/src/fast_cached_image.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'dart:ui' as ui;
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:uuid/uuid.dart';
 import 'models/fast_cache_progress_data.dart';
@@ -219,65 +220,88 @@ class _FastCachedImageState extends State<FastCachedImage>
                       return widget.loadingBuilder!(context, _progressData);
                     })
                 : const SizedBox(),
-          if (_imageResponse != null)
-            FadeTransition(
-              opacity: _animation,
-              child: Image.memory(
-                _imageResponse!.imageData,
-                color: widget.color,
-                width: widget.width,
-                height: widget.height,
-                alignment: widget.alignment,
-                key: widget.key,
-                cacheWidth: widget.cacheWidth,
-                cacheHeight: widget.cacheHeight,
-                fit: widget.fit,
-                errorBuilder: (a, c, v) {
-                  if (_animationController.status !=
-                      AnimationStatus.completed) {
-                    _animationController.forward();
-                    _logErrors(c);
-                    FastCachedImageConfig.deleteCachedImage(
-                        imageUrl: widget.url, showLog: widget.showErrorLog);
-                  }
-                  return widget.errorBuilder != null
-                      ? widget.errorBuilder!(a, c, v)
-                      : const SizedBox();
-                },
-                centerSlice: widget.centerSlice,
-                colorBlendMode: widget.colorBlendMode,
-                excludeFromSemantics: widget.excludeFromSemantics,
-                filterQuality: widget.filterQuality,
-                gaplessPlayback: widget.gaplessPlayback,
-                isAntiAlias: widget.isAntiAlias,
-                matchTextDirection: widget.matchTextDirection,
-                opacity: widget.opacity,
-                repeat: widget.repeat,
-                scale: widget.scale,
-                semanticLabel: widget.semanticLabel,
-                frameBuilder: (widget.loadingBuilder != null)
-                    ? (context, a, b, c) {
-                        if (b == null) {
-                          return widget.loadingBuilder!(
-                              context,
-                              FastCachedProgressData(
-                                  progressPercentage:
-                                      _progressData.progressPercentage,
-                                  totalBytes: _progressData.totalBytes,
-                                  downloadedBytes:
-                                      _progressData.downloadedBytes,
-                                  isDownloading: false));
-                        }
 
-                        if (_animationController.status !=
-                            AnimationStatus.completed) {
-                          _animationController.forward();
-                        }
-                        return a;
-                      }
-                    : null,
+          if (_imageResponse != null)...[
+
+            if(widget.url.toString().split(".").last == 'svg')...[
+              FadeTransition(
+                opacity: _animation,
+                child: SvgPicture.memory(
+                  _imageResponse!.imageData,
+                  color: widget.color,
+                  width: widget.width,
+                  height: widget.height,
+                  alignment: widget.alignment,
+                  key: widget.key,
+                  fit: widget.fit ?? BoxFit.contain,
+                  colorBlendMode: widget.colorBlendMode ??  ui.BlendMode.srcIn,
+                  excludeFromSemantics: widget.excludeFromSemantics,
+                  matchTextDirection: widget.matchTextDirection,
+                ),
+              )
+            ]else...[
+              FadeTransition(
+                opacity: _animation,
+                child: Image.memory(
+                  _imageResponse!.imageData,
+                  color: widget.color,
+                  width: widget.width,
+                  height: widget.height,
+                  alignment: widget.alignment,
+                  key: widget.key,
+                  cacheWidth: widget.cacheWidth,
+                  cacheHeight: widget.cacheHeight,
+                  fit: widget.fit,
+                  errorBuilder: (a, c, v) {
+                    if (_animationController.status !=
+                        AnimationStatus.completed) {
+                      _animationController.forward();
+                      _logErrors(c);
+                      FastCachedImageConfig.deleteCachedImage(
+                          imageUrl: widget.url, showLog: widget.showErrorLog);
+                    }
+                    return widget.errorBuilder != null
+                        ? widget.errorBuilder!(a, c, v)
+                        : const SizedBox();
+                  },
+                  centerSlice: widget.centerSlice,
+                  colorBlendMode: widget.colorBlendMode,
+                  excludeFromSemantics: widget.excludeFromSemantics,
+                  filterQuality: widget.filterQuality,
+                  gaplessPlayback: widget.gaplessPlayback,
+                  isAntiAlias: widget.isAntiAlias,
+                  matchTextDirection: widget.matchTextDirection,
+                  opacity: widget.opacity,
+                  repeat: widget.repeat,
+                  scale: widget.scale,
+                  semanticLabel: widget.semanticLabel,
+                  frameBuilder: (widget.loadingBuilder != null)
+                      ? (context, a, b, c) {
+                    if (b == null) {
+                      return widget.loadingBuilder!(
+                          context,
+                          FastCachedProgressData(
+                              progressPercentage:
+                              _progressData.progressPercentage,
+                              totalBytes: _progressData.totalBytes,
+                              downloadedBytes:
+                              _progressData.downloadedBytes,
+                              isDownloading: false));
+                    }
+
+                    if (_animationController.status !=
+                        AnimationStatus.completed) {
+                      _animationController.forward();
+                    }
+                    return a;
+                  }
+                      : null,
+                ),
               ),
-            ),
+            ]
+
+
+          ]
         ],
       ),
     );
